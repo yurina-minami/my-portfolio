48fe6cb57271237a7d6419a346c570c80c306a91
yurina-minami <yurinatty373@gmail.com>
2023-01-13 14:56:41 +0900

    詳細ページ実装

--- js/main.js
+++ js/main.js
@@ -13,4 +13,33 @@ $(function () {
     $(".hamburger").toggleClass("active");
     $(".nav").toggleClass("active");
   });
+
+  $(".inview1").on("inview", function () {
+    $(".right1").toggleClass("show");
+  });
+
+  $(".inview2").on("inview", function () {
+    $(".right2").toggleClass("show");
+  });
+
+  $(".inview3").on("inview", function () {
+    $(".right3").toggleClass("show");
+  });
+
+  $(".inview4").on("inview", function () {
+    $(".right3").toggleClass("show");
+    $(".right1").toggleClass("show");
+  });
+
+  $(".inview5").on("inview", function () {
+    $(".right5").toggleClass("show");
+  });
+
+  $(".inview6").on("inview", function () {
+    $(".right6").toggleClass("show");
+  });
+
+  $(".inview7").on("inview", function () {
+    $(".right6").toggleClass("show");
+  });
 });
